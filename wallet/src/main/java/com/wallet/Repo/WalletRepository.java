830d30f825e795bd2a9eb9d3b4ba7b8296cae873
Mohammed Usman <95905479+usmansmg25@users.noreply.github.com>
2023-01-16 17:40:51 +0530

    Add files via upload
    got it

---
+++ wallet/src/main/java/com/wallet/Repo/WalletRepository.java
@@ -0,0 +1,11 @@
+package com.wallet.Repo;
+
+
+import com.wallet.dto.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet, Integer> {
+
+}
